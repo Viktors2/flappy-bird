fdcdbb2c799f751432879e2db9610688333a6b8d
Viktors2 <vitjok22@gmail.com>
2023-05-29 23:04:14 +0200

    collision q

--- flappy-script.js
+++ flappy-script.js
@@ -101,3 +101,10 @@ function birdMoves(e) {
         velocityY = -6;
     }
 }
+
+function detectCollision(a, b) {
+    return a.x < b.x + b.width &&
+        a.x + a.width > b.x &&
+        a.y < b.y + b.height &&
+        a.y + a.height > b.y;
+}
